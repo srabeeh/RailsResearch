95bd34ee7a8ce5ebcef2635caa97297e14b5add5
Sam Rabeeh <srabeeh@larci.com>
2015-11-23 23:32:02 -0700

    Crud finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,6 +33,13 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+	
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end
+	
 	private
 		def article_parameters
 			params.require(:article).permit(:title, :text)
