cf38494b96b02d7ed51ce7128b3a5e16c6e44c83
Sam Rabeeh <srabeeh@larci.com>
2015-11-23 23:07:45 -0700

    Crud created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,33 @@
 class ArticlesController < ApplicationController
 	def new
+		@article = Article.new
+	end
+	
+	def edit
+		@article = Article.find(params[:id])
+	end
+	
+	def index
+		@articles = Article.all
 	end
 	
 	def create
 		@article = Article.new(article_parameters)
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+	
+	def update
+		@article = Article.find(params[:id])
+		
+		if @article.update(article_parameters)
+			redirect_to @article
+		else
+			render 'edit'
+		end
 	end
 	
 	def show
